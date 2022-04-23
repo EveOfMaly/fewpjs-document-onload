530a51e5579e09de268e0823e45cf8b2d5661335
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-04-23 15:45:14 -0400

    Test not working but working in prod

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(){
+    theDomHasLoaded() 
+});
+
+
+function theDomHasLoaded() {
+    document.getElementById('text').innerText = "This is really cool!";
+}
+
